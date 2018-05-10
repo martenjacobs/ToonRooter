8a427968d0810069dcee8517487eb06ecd62a12d
TheHogNL <igor@ergens.org>
2018-05-10 08:11:11 +0200

    change default password from none to 'password'

--- payload/patch_toon.sh
+++ payload/patch_toon.sh
@@ -7,7 +7,7 @@ echo ">>> Enabling root user"
 if [[ ! -f /etc/passwd.preroot ]]; then
   cp /etc/passwd /etc/passwd.preroot
 fi
-sed -i -e 's/^root:DISABLED:/root::/' /etc/passwd
+sed -i -e 's/^root:DISABLED:/root:uAMdksdtrttj2:/' /etc/passwd
 
 echo ">>> Opening ports 22, 80 and 10080 on firewall"
 if [[ ! -f /etc/default/iptables.conf.preroot ]]; then
